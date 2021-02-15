44a2c285ac40ba896137eff6c1cf3aa1402d947b
Mohit Ahirwar <66860602+MohitAhirwar@users.noreply.github.com>
2021-02-16 01:48:57 +0530

    Create LoopinLinkedList.cpp

---
+++ LoopinLinkedList.cpp
@@ -0,0 +1,28 @@
+/*
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+
+bool detectLoop(Node* head)
+{
+    unordered_set<Node*> s;
+    while (head != NULL) {
+      
+        if (s.find(head) != s.end())
+            return true;
+ 
+        s.insert(head);
+ 
+        head = head->next;
+    }
+ 
+    return false;
+}
